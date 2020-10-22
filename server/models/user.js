1b5fe01ebd56607791283bc895e88b8a09c2fce9
ShadeB <shadebanda2@gmail.com>
2020-10-22 12:34:53 +0200

    Add  a pre save hook on the user model to hash passwords before they are saved to the database

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,26 @@
-import { mongoose, Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+import { hash } from 'bcrypt';
 
 const userSchema = new Schema({
-  _id: { type: Schema.Types.ObjectId },
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   admin_user: { type: Boolean },
+});
+
+// eslint-disable-next-line
+userSchema.pre('save', async function (next) {
+  const document = this;
+
+  // only hash the password if the document is new
+  // or password has been changed
+  if (document.isNew || document.isModified('password')) {
+    const hashedPassword = await hash(document.password, 12);
+    document.password = hashedPassword;
+    next();
+  } else {
+    return next();
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
