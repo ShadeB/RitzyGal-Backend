21e959aa8e8521ba33ecadad2a8809e85a6c79ef
ShadeB <shadebanda2@gmail.com>
2020-10-22 13:16:44 +0200

    closes #7, add graphql config to allow user login and signup

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ import express, { json, urlencoded } from 'express';
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import { graphqlHTTP } from 'express-graphql';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+
+import schema from './graphql/schemas/user';
+
+import rootResolver from './graphql/resolvers/index';
 
 const app = express();
 
@@ -16,5 +21,14 @@ app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: rootResolver,
+    graphiql: true,
+  }),
+);
 
 export default app;
