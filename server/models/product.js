f00c193e3ff1bfc67e26f1e5dabdf90a55ec31c1
ShadeB <shadebanda2@gmail.com>
2020-10-28 18:47:40 +0200

    Create product model

---
+++ server/models/product.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose';
+
+const ProductSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: [String], required: true },
+  price: { type: mongoose.Types.Decimal128, required: true },
+  sizes: { type: String, required: true },
+  image: { type: String, required: true },
+  brand: { type: String, required: true },
+  instock: { type: Boolean, required: true },
+  rating: { type: mongoose.Types.Decimal128, required: true },
+  colors: { type: [{ name: String, hex: String }], required: true },
+});
+
+module.exports(mongoose.model('Product', ProductSchema));
