0ca0d5f6da5887ab21a9b04a427354c65e274670
ShadeB <shadebanda2@gmail.com>
2020-11-03 17:06:14 +0200

    add tests suite

---
+++ tests/createProduct.test.js
@@ -0,0 +1,8 @@
+import requestLib from 'supertest';
+import mongoose from 'mongoose';
+import chai, { expect } from 'chai';
+
+const url = 'http://localhost:3000/graphql';
+const request = requestLib(url);
+
+describe('createProduct', () => {});
