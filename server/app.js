122b35ae7f960cb5bb067d70efeda486af61074b
ShadeB <shadebanda2@gmail.com>
2020-11-08 23:53:25 +0200

    Allow cross origin requests

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import { join } from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { graphqlHTTP } from 'express-graphql';
+import cors from 'cors';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
@@ -19,6 +20,7 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, 'public')));
 
+app.use(cors);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
